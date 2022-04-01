4ab48de9360f1af0d0bc6b33effe1e5755c04fd0
Patrick Brennan <brennanp432@gmail.com>
2022-04-01 17:19:09 -0400

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,67 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Patrick Brennan",
+    "role": "Software Developer",
+    "linkedinId":"718241145",
+    "roleDescription": " I enjoy dabbling in various parts of development and love to learn about new technologies or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/patrick-brennan-718241145/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Brennanp432",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a Software Developer at ATR, Inc. I am in my final year as a student at the College of Central Florida. I am a self taught Full Stack Developer, currently diving deeper into mastering React.",
+    "address":"Florida, United States",
+    "phone":"352-613-2462",
+    "education":[
+      {
+        "UniversityName":"College of Central Florida",
+        "specialization":"Management Information Systems",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2022",
+        "Achievements":"Dean's List for all semesters"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Applied Technology Resources, Inc.",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Todo App",
+        "description":"React Todo App",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"Patrick just knows what to do, no matter what the task, he is always up for it.",
+        "name":"Former Supervisor"
+      }
+    ]
+  }
+  
+  export default resumeData
